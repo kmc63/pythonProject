8f36495e45858a0b3d5fd882c43f3c4ff2a93992
Keith Cicala <keithcicala123@gmail.com>
2022-02-07 18:40:03 -0500

    Changed name on index page

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Keith Cicala"
         return render_template('index.html', name=name)
